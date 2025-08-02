24144065b0f95bb5bd222bdf2f2f9e02f8097c68
David Galstyan <galstyand.25@gmail.com>
2025-08-02 15:11:47 +0400

    THird COmmit

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,39 +1,8 @@
 import jetbrains.buildServer.configs.kotlin.*
-import jetbrains.buildServer.configs.kotlin.buildSteps.script
 
 version = "2022.10"
 
 project {
-    buildType(BuildAndDockerize)
+    description = "Just a test project"
 }
-
-object BuildAndDockerize : BuildType({
-    name = "Build and Dockerize"
-
-    vcs {
-        root(DslContext.settingsRoot)
-    }
-
-    steps {
-        script {
-            name = "Say hello"
-            scriptContent = "echo 'Building app...'"
-        }
-
-        script {
-            name = "Docker build"
-            scriptContent = """
-                docker build -t my-app:latest .
-            """.trimIndent()
-        }
-    }
-
-    artifacts {
-        artifactRules = "Dockerfile"
-    }
-
-    triggers {
-        vcs {}
-    }
-})
 
