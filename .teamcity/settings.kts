dbe5aa04af46b511fbb148ece37116e8f26eea4f
David Galstyan <galstyand.25@gmail.com>
2025-08-02 15:19:52 +0400

    Forth COmmit

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,8 +1,39 @@
 import jetbrains.buildServer.configs.kotlin.*
+import jetbrains.buildServer.configs.kotlin.buildSteps.script
 
 version = "2022.10"
 
 project {
-    description = "Just a test project"
+    buildType(BuildAndDockerize)
 }
+
+object BuildAndDockerize : BuildType({
+    name = "Build and Dockerize"
+
+    vcs {
+        root(DslContext.settingsRoot)
+    }
+
+    steps {
+        script {
+            name = "Say hello"
+            scriptContent = "echo 'Building app...'"
+        }
+
+        script {
+            name = "Docker build"
+            scriptContent = """
+                docker build -t my-app:latest .
+            """.trimIndent()
+        }
+    }
+
+    artifacts {
+        artifactRules = "Dockerfile"
+    }
+
+    triggers {
+        vcs {}
+    }
+})
 
