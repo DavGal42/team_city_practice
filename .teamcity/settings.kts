6ec65a9d09bebdbc078dd215f0b97752e6d7ed40
David Galstyan <galstyand.25@gmail.com>
2025-08-02 12:46:46 +0400

    Second COmmit

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,36 +1,39 @@
+import jetbrains.buildServer.configs.kotlin.*
+import jetbrains.buildServer.configs.kotlin.buildSteps.script
+
 version = "2022.10"
 
 project {
-    buildType {
-        id("BuildAndDockerize")
-        name = "Build and Dockerize"
+    buildType(BuildAndDockerize)
+}
 
-        vcs {
-            root(DslContext.settingsRoot)
+object BuildAndDockerize : BuildType({
+    name = "Build and Dockerize"
+
+    vcs {
+        root(DslContext.settingsRoot)
+    }
+
+    steps {
+        script {
+            name = "Say hello"
+            scriptContent = "echo 'Building app...'"
         }
 
-        steps {
-            script {
-                name = "Say hello"
-                scriptContent = "echo 'Building app...'"
-            }
-
-            script {
-                name = "Docker build"
-                scriptContent = """
-                    docker build -t my-app:latest .
-                """.trimIndent()
-            }
-        }
-
-        artifacts {
-            artifactRules = "Dockerfile"
-        }
-
-        triggers {
-            vcs {
-            }
+        script {
+            name = "Docker build"
+            scriptContent = """
+                docker build -t my-app:latest .
+            """.trimIndent()
         }
     }
-}
+
+    artifacts {
+        artifactRules = "Dockerfile"
+    }
+
+    triggers {
+        vcs {}
+    }
+})
 
