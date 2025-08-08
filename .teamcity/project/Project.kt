5b8376ff2810413835c1e94003bbfb162774043b
David Galstyan <galstyand.25@gmail.com>
2025-08-08 15:48:03 +0400

    Change ./teamciy directory content

---
+++ .teamcity/project/Project.kt
@@ -0,0 +1,24 @@
+package project
+
+import jetbrains.buildServer.configs.kotlin.*
+import jetbrains.buildServer.configs.kotlin.buildSteps.script
+import jetbrains.buildServer.configs.kotlin.project
+import jetbrains.buildServer.configs.kotlin.BuildType
+
+object Project : Project({
+    name = "HelloWorldProject"
+
+    buildType(HelloWorldBuild)
+})
+
+object HelloWorldBuild : BuildType({
+    name = "Hello World Build"
+
+    steps {
+        script {
+            name = "Print Hello World"
+            scriptContent = """echo "hello world""""
+        }
+    }
+})
+
